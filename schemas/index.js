51ee7fc373a54c927f1e727702393a6e0b218b0e
Shittu24 <ibrahimshittu247@gmail.com>
2023-01-03 11:31:15 -0800

    backend commit

---
+++ schemas/index.js
@@ -0,0 +1,18 @@
+export const schemaTypes = [
+    works,
+    testimonials,
+    brands,
+    about,
+    skills,
+    workExperience,
+    experience,
+    contact,
+]
+import testimonials from "./testimonials"
+import contact from "./contact"
+import about from "./about"
+import brands from "./brands"
+import experience from "./experience"
+import skills from "./skills"
+import workExperience from "./workExperience"
+import works from "./works"
